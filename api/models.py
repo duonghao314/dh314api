cf7679b7a9beabd21b347d5ec2c4a08488e9f61d
duonghao314 <48150245+duonghao314@users.noreply.github.com>
2019-08-23 10:16:03 +0700

    ssss

--- api/models.py
+++ api/models.py
@@ -56,7 +56,7 @@ class Profile(models.Model):
     address = models.CharField(max_length=100,blank=True)
     country = models.CharField(max_length=32,blank=True)
     phone = models.CharField(max_length=15,blank=True)
-    date_of_birth = models.DateField(default = )
+    date_of_birth = models.DateField(blank=True)
 
     def __str__(self):
         return self.uuid
