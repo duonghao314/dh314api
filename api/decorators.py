81bd8720f5acdac83f0a7bc1fddedfd2ec89f765
duonghao314 <48150245+duonghao314@users.noreply.github.com>
2019-08-28 16:03:00 +0700

    fix decorators

--- api/decorators.py
+++ api/decorators.py
@@ -4,9 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 
 def token_not_in_blacklist(func):
-    def wrapper(request):
+    def wrapper(request, *args, **kwargs):
         if check_blacklist_token(request):
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         else:
-            return func(request)
+            return func(request, *args, **kwargs)
     return wrapper
