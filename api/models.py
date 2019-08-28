fc6745d0b043211f05f6e25d75b72c2f743a279d
duonghao314 <48150245+duonghao314@users.noreply.github.com>
2019-08-28 15:19:30 +0700

    a few changes

--- api/models.py
+++ api/models.py
@@ -52,7 +52,7 @@ class Account(AbstractBaseUser, PermissionsMixin):
 
 class Profile(models.Model):
     id = models.IntegerField(primary_key=True, unique=True, auto_created=True)
-    uuid = models.TextField(unique=True, default='')
+    uuid = models.TextField(unique=True)
     fullname = models.CharField(max_length=50, blank=True)
     address = models.CharField(max_length=100, blank=True)
     country = models.CharField(max_length=32, blank=True)
